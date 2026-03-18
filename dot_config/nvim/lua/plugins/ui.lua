a3382fba46650b50b14d38b53aaf01cab805fe6e
RemiGirard <remi.girard@protonmail.com>
2026-03-18 11:00:07 +0100

    feat: move nvim statusline to top via winbar

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -9,4 +9,18 @@ return {
       stop_eof = true,
     },
   },
+  -- Move statusline to top
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      opts.options = opts.options or {}
+      opts.options.disabled_filetypes = opts.options.disabled_filetypes or {}
+      opts.options.disabled_filetypes.winbar = { "snacks_dashboard", "neo-tree", "lazy", "mason" }
+      -- Move sections from statusline to winbar
+      opts.winbar = opts.sections
+      opts.sections = {}
+      opts.options.globalstatus = true
+      vim.o.laststatus = 0
+    end,
+  },
 }
