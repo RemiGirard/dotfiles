886a607ef10ec6a38566b7a44e3385817545609b
RemiGirard <remi.girard@protonmail.com>
2026-03-18 11:02:40 +0100

    revert: remove broken winbar config, keep default statusline

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -9,22 +9,4 @@ return {
       stop_eof = true,
     },
   },
-  -- Move statusline to top via winbar
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = function(_, opts)
-      local sections = vim.deepcopy(opts.sections or {})
-      opts.winbar = sections
-      opts.inactive_winbar = {
-        lualine_c = { "filename" },
-      }
-      opts.sections = {}
-      opts.inactive_sections = {}
-      opts.options = opts.options or {}
-      opts.options.globalstatus = false
-      opts.options.disabled_filetypes = opts.options.disabled_filetypes or {}
-      opts.options.disabled_filetypes.winbar = { "snacks_dashboard", "neo-tree", "lazy", "mason", "toggleterm", "Trouble" }
-      vim.o.laststatus = 0
-    end,
-  },
 }
